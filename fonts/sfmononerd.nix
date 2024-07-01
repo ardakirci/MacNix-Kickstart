203c7c4323dfa4f3db2b3110147ed9c35ef14317
ardakirci <krcarda@gmail.com>
2024-07-01 13:36:53 +0300

    update

--- fonts/sfmononerd.nix
+++ fonts/sfmononerd.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   fonts = {
-    fontDir.enable = true;
+    # fontDir.enable = true;
     packages = with pkgs; [ 
       sf-mono-liga-bin
       (nerdfonts.override { fonts = [ "Hack" ]; }) 
